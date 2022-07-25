242811f1bcd63458e426ca60a8676eb275a688e1
Thaibm <thai.buiminh@ncc.asia>
2022-07-25 17:37:21 +0700

    Update requirement and add example file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleCommonComponentComponent } from './common/components/example-common-component/example-common-component.component';
+import { ExamplePipePipe } from './common/pipe/example-pipe.pipe';
+import { ExampleDirectiveDirective } from './common/directives/example-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleCommonComponentComponent,
+    ExamplePipePipe,
+    ExampleDirectiveDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/components/example-common-component/example-common-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example-common-component',
+  templateUrl: './example-common-component.component.html',
+  styleUrls: ['./example-common-component.component.scss']
+})
+export class ExampleCommonComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
